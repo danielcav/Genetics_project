70dc99ceccc5a618fe8ffbdd63e5d150a8a767cb
danielcav <daniel.cavaleri@epfl.ch>
2021-12-06 17:56:53 +0100

    on est chaud sa mere

--- R_script.R
+++ R_script.R
@@ -11,6 +11,7 @@ genotypes.clean <- genotypes.vcf[2:nrow(genotypes.vcf),10:ncol(genotypes.vcf)]
 call.rate <- apply(genotypes.clean, 1, function(x) sum(!is.na(x))/ncol(genotypes.clean))
 #We keep the variants with a 100% call rate.
 variants.clean = call.rate[call.rate == 1.]
+genotypes.without.na <- genotypes.clean[rownames(genotypes.clean)%in%names(variants.clean),]
 #We plot the histogram.
 call.rate <- as.data.frame(call.rate)
 call.rate$idx = 1:25000
@@ -25,13 +26,14 @@ minors <- function (x){
   zero.zero = length(which(x== "0/0"))
   nb.zero = zero.zero*2 + one.zero
   nb.one = one.one*2 + one.zero
-  if(nb.zero == 0){return(1)}
+  if(nb.zero == 0){return(0)}
   if (nb.zero<nb.one){
     return((nb.zero)/(2*sum(!is.na(x))))
   }else{return(nb.ones/(2*sum(!is.na(x))))}
 }
 counted <- apply(genotypes.clean, 1, function(x) (minors(x)))
 counted.clean = counted[counted > 0.01]
+genotypes.filtered <- genotypes.without.na[rownames(genotypes.without.na)%in%names(counted.clean),]
 print(paste("We've removed :", 25000-length(counted.clean),"variants"))
 cou<-as.data.frame(counted)
 ggplot(data = cou, mapping=aes(x=counted)) + geom_histogram(binwidth = 0.01)
@@ -44,45 +46,32 @@ print("Since the R-squared is close to 0, we can assume that there is no obvious
 #Boxplot
 ggplot(data = linmodel, mapping=aes(y=Cholesterol,x=gender)) + geom_boxplot()
 ggplot(linmodel, aes(Cholesterol)) + geom_density(aes(col = 'all')) + geom_density(aes(col = gender)) + geom_density()
-print("No because the distribution is homogenous to a normal distribution, which is random")
+print("No because the distribution is homogenous to a normal distribution, which is random.")
 #1 = alt, 0 = ref
-data.for.pca = t(genotypes.clean)
-colnames(data.for.pca) = genotypes.vcf$V3[2:25001]
+data.for.pca = as.data.frame(t(genotypes.filtered))
 data.for.pca[data.for.pca == "0/0"] = 0
 data.for.pca[data.for.pca == "0/1"] = 1
+data.for.pca[data.for.pca == "1/0"] = 1
 data.for.pca[data.for.pca == "1/1"] = 2
-data.for.pca[is.na(data.for.pca)] <- 3
-str(data.for.pca)
-storage.mode(data.for.pca) <- "numeric"
-str(data.for.pca)
-data.pca = prcomp(data.for.pca, center = T)
-#data.plot = data.pca$x
-#plot(data.plot[,1], data.plot[,2])
-
-df <- data.frame(data.pca$x)
-ggplot(df,aes(x=PC1,y=PC2)) + geom_point()
+val <- as.matrix(data.for.pca)
+storage.mode(val) <- "numeric"
+data.pca = prcomp(val)
+df <- data.pca$x
+ggplot(as.data.frame(df),aes(x=PC1,y=PC2)) + geom_point()
 #GWAS:
-#On enlève tous les éléments où y a pas de variations:
-data.for.gwas <- apply(genotypes.clean, 1, function(x) minors(x) != 1)
-#la le probleme cest que c'est que des true/false et pas directement 
-#les lignes de genotype.clean
-#La j'ajoute le nom des colonnes
-colnames(genotypes.clean) <- genotypes.vcf[1, 10:ncol(genotypes.vcf)]
-#la je crée un fichier clean juste avec les valeurs du cholesterol
-#et je donne un nom explicite aux lignes/colonnes
-phenotypes.clean <- as.data.frame(phenotypes.txt$Cholesterol)
-colnames(phenotypes.clean) <- "Phenotype"
-rownames(phenotypes.clean) <- phenotypes.txt$sample_id
-#a partir de la c'est le bad
-#j'ai essayé de faire une fonction "model" qui va merge les lignes 
-#et qui applique la fonction lm()
-#elle marche pas encore parce que faut mettre un truc dans lm("")
-#mais le merge en soit marche bien
-apply(genotypes.clean, 1, function(x) if(data.for.gwas[rownames(x)]){return(summary(lm("Phenotype ~ Variant", data = c)))})
-model <- function(x){
-  a <- t(x)
-  b <- phenotypes.clean
-  c <- cbind(a,b)
-  lm("" ,data = c)
+data.for.gwas <- as.data.frame(t(data.for.pca))
+beta_p <- function(x){
+  temp <- summary(lm(phenotypes.txt$Cholesterol ~ as.numeric(x)))
+  return(c(temp$coefficients[2,1],-log10(temp$coefficients[2,4])))#beta,pvalue
 }
-data.for.gwas <- apply(genotypes.clean, 1, function(x) summary(lm(data = cbind(x, phenotypes.clean))))
+data.gwas <- apply(data.for.gwas, 1, function(x) beta_p(x))
+data.gwas <- rbind(data.gwas,c(1:ncol(data.gwas)))
+chr.pos <- genotypes.vcf[rownames(genotypes.vcf) %in% colnames(data.gwas),1]
+data.gwas <- rbind(data.gwas,as.numeric(chr.pos))
+data.gwas.t <- as.data.frame(t(data.gwas))
+treshold <- -log10(0.05/nrow(data.gwas.t))
+data.gwas.t$color[(data.gwas.t$V4 %% 2) == 0] = "odd"
+data.gwas.t$color[(data.gwas.t$V4 %% 2) != 0] = "even"
+data.gwas.t$color[data.gwas.t$V2 > treshold] = "significant"
+ggplot(as.data.frame(data.gwas.t),aes(x=V3, y=V2, col = factor(color))) + geom_point() + geom_hline(yintercept=treshold, linetype="dashed", color = "red")
+#6
